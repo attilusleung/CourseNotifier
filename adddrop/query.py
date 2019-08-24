8216e9a11cd5b0a1ee9822b76605652c7bb027c2
attilusleung <attilusleung@gmail.com>
2019-08-24 11:55:16 -0400

    cleaned up syntax

--- adddrop/query.py
+++ adddrop/query.py
@@ -5,7 +5,6 @@ import os
 
 from .logger import get_logger
 
-
 BASE_LINK = "https://classes.cornell.edu/browse/roster"
 SEMESTER = "FA19"
 COURSE = "CS 3410"
@@ -13,34 +12,36 @@ COURSE = "CS 3410"
 LOG_NAME = "query.log"
 logger = get_logger(__name__, LOG_NAME)
 
-
 Section = namedtuple('Section', ['nbr', 'number', 'time'])
 Section.__str__ = lambda self: f"[{self.number}]{self.nbr}: {self.time}"
 
+
 def find_openings(course, semester=SEMESTER):
-    link = "/".join([BASE_LINK, SEMESTER, "class", course.replace(' ', '/')])
+    link = "/".join([BASE_LINK, semester, "class", course.replace(' ', '/')])
     r = requests.get(link)
     page = BeautifulSoup(r.text, features="html.parser")
 
     assert page.find_all("h1", {"class": "sr-only"}), \
         ("Something went wrong. We accessed a "
-        "different course than the one specified.")
+         "different course than the one specified.")
 
     section = page.find_all("ul", {"class": "section"})
     openings = {}
     for s in section:
         if s.find("li", {"class": "open-status"}).\
-                find("span", {"class":"tooltip-iws"})['data-content'] != "Open":
+                find("span", {"class": "tooltip-iws"})['data-content'] != "Open":
             continue
-        component = s.find("em", {"title":"Component"}).string
-        nbr = s.find("strong", {"title":"Class Number"}).string
-        number = s['aria-label'].replace('Class Section', '').replace(component, '').strip()
+        component = s.find("em", {"title": "Component"}).string
+        nbr = s.find("strong", {"title": "Class Number"}).string
+        number = s['aria-label'].replace('Class Section',
+                                         '').replace(component, '').strip()
         datetime = s.find("span", {"class": "pattern"})
         time = f'{datetime.find("span", {"class":"tooltip-iws"}).string} {datetime.find("time").string}'
         if component not in openings.keys():
             openings[component] = [Section(nbr=nbr, number=number, time=time)]
         else:
-            openings[component].append(Section(nbr=nbr, number=number, time=time))
+            openings[component].append(
+                Section(nbr=nbr, number=number, time=time))
 
     return openings
 
@@ -51,6 +52,6 @@ def find_all_openings(courses, semester=SEMESTER):
     for c in courses:
         try:
             openings[c] = find_openings(c, semester)
-        except Exception as e:
+        except Exception:
             logger.exception('Skpping unparsable course %s.', c)
     return openings
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
 from configparser import ConfigParser
+
 import json
 from io import StringIO
 import os
@@ -20,9 +21,11 @@ DEPENDENCIES_PATH = ['adddrop/query.py', 'adddrop/logger.py']
 # TODO: logs
 logger = get_logger(__name__, LOG_NAME)
 
-
-argparser = ArgumentParser(description='Checks availability of Cornell classes')
-# argparser.add_argument('--classes', nargs='+', default=None, help='Read from list of class codes')
+argparser = ArgumentParser(
+    description='Checks availability of Cornell classes')
+# argparser.add_argument(
+#   '--classes', nargs='+',
+#   default=None, help='Read from list of class codes')
 argparser.add_argument('--sem', default="", help='Use semester code provided')
 args = argparser.parse_args()
 
@@ -36,6 +39,7 @@ if not classes or not sem:
     assert confparser.read(CONFIG_PATH), "Failed to read config file"
 
     if not classes:
+
         def parse_config(section):
             ret = {}
             for k, v in confparser[section].items():
@@ -46,7 +50,8 @@ if not classes or not sem:
         sections = confparser.sections()
         try:
             sections.remove('SETTINGS')
-        except ValueError: pass
+        except ValueError:
+            pass
         for s in sections:
             classes[s] = parse_config(s)
 
@@ -54,9 +59,9 @@ if not classes or not sem:
 
 logger.info('Watching classes %s from %s', str(classes.keys()), sem)
 
-
 WATCHED_FILES = [CONFIG_PATH, SECRETS_PATH, __file__, *DEPENDENCIES_PATH]
 WATCHED_FILES_MTIMES = [(f, os.path.getmtime(f)) for f in WATCHED_FILES]
+
 
 def check_mtimes():
     for f, m in WATCHED_FILES_MTIMES:
@@ -71,29 +76,39 @@ message_client = Client(secrets['SID'], secrets['AUTH'])
 msg_builder = ConfigParser()
 msg_builder.optionxform = lambda option: option
 
+
 def intersect_class_dicts(openings, class_dict):
-    return dict([(x, [y for y in openings[x] if y.number in class_dict[x]]) for x in openings.keys() if x in class_dict])
+    return dict([(x, [y for y in openings[x] if y.number in class_dict[x]])
+                 for x in openings.keys() if x in class_dict])
+
 
 def minus_dicts(dict1, dict2):
-    return dict([(x, [y for y in dict1[x] if y not in dict2[x]]) if x in dict2 else (x, dict1[x]) for x in dict1.keys()])
+    return dict([(x, [y for y in dict1[x]
+                      if y not in dict2[x]]) if x in dict2 else (x, dict1[x])
+                 for x in dict1.keys()])
+
 
 def str_dict_items(d):
-    return dict([(k, "\n" + "\n".join([str(i) for i in v])) for k, v in d.items()])
+    return dict([(k, "\n" + "\n".join([str(i) for i in v]))
+                 for k, v in d.items()])
+
 
 def str_all(d):
     return dict([(k, str_dict_items(v)) for k, v in d.items()])
 
 
 prev = {}
-while(True):
+while (True):
     try:
         openings = find_all_openings(classes.keys(), sem)
         # print(openings)
 
-        classes_open = dict([(c, intersect_class_dicts(s, classes[c])) for c, s in openings.items() if c in classes])
+        classes_open = dict([(c, intersect_class_dicts(s, classes[c]))
+                             for c, s in openings.items() if c in classes])
         # print(classes_open)
 
-        # if any([all([s for s in c.values()]) for c in classes_open.values() if c]):
+        # if any([all([s for s in c.values()])
+        #         for c in classes_open.values() if c]):
         #     logger.info('there are openings in at least 1 class!')
 
         if classes_open != prev:
@@ -103,7 +118,8 @@ while(True):
             sio = StringIO()
             msg_builder.write(sio)
 
-            changes = dict([(c, minus_dicts(s, prev[c])) if c in prev else (c, s) for c, s in classes_open.items()])
+            changes = dict([(c, minus_dicts(s, prev[c])) if c in prev else
+                            (c, s) for c, s in classes_open.items()])
             msg_builder.clear()
             msg_builder.read_dict(str_all(changes))
             ssio = StringIO()
@@ -111,24 +127,22 @@ while(True):
 
             logger.info(sio.getvalue())
             message = message_client.messages.create(
-                    body= \
-                        f"Change in tracked classes \n\nChanges: \n{ssio.getvalue()} \n\nAll Open Classes: \n{sio.getvalue()}",
-                    from_=confparser['TWILIO']['SOURCE_PHONE'],
-                    to=confparser['TWILIO']['DEST_PHONE']
-                    )
+                body=(f"Change in tracked classes "
+                      "\n\nChanges: \n{ssio.getvalue()} "
+                      "\n\nAll Open Classes: \n{sio.getvalue()}"),
+                from_=confparser['TWILIO']['SOURCE_PHONE'],
+                to=confparser['TWILIO']['DEST_PHONE'])
             logger.info("Set message @ %s", message.sid)
             prev = classes_open
 
         sleep(5)
         check_mtimes()
-    except Exception as e:
+    except Exception:
         logger.exception('Unhandled exception during main loop')
         from traceback import format_exc
         message = message_client.messages.create(
-                body= \
-                f"Unhandled exception during main loop \n\n{format_exc()}",
-                from_=confparser['TWILIO']['SOURCE_PHONE'],
-                to=confparser['TWILIO']['DEST_PHONE']
-                )
+            body=f"Unhandled exception during main loop \n\n{format_exc()}",
+            from_=confparser['TWILIO']['SOURCE_PHONE'],
+            to=confparser['TWILIO']['DEST_PHONE'])
         logger.error('Stacktrace sent as sms')
         raise
